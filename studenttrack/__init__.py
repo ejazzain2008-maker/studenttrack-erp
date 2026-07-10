b871b5843c34dfc2ea96eacd12e56f7ad2723e4b
fauziaejaz9-ux <fauziaejaz9@gmail.com>
2026-07-10 17:27:51 +0530

    Move pymysql database patches to settings.py to ensure execution

--- studenttrack/__init__.py
+++ studenttrack/__init__.py
@@ -1,11 +1,1 @@
-import pymysql
-pymysql.install_as_MySQLdb()
 
-# Monkey-patch Django to bypass the MariaDB 10.6 version check
-from django.db.backends.base.base import BaseDatabaseWrapper
-BaseDatabaseWrapper.check_database_version_supported = lambda self: None
-
-# Monkey-patch Django's MySQL features to disable the RETURNING clause for MariaDB 10.4 compatibility
-from django.db.backends.mysql.features import DatabaseFeatures
-DatabaseFeatures.can_return_columns_from_insert = property(lambda self: False)
-DatabaseFeatures.can_return_rows_from_bulk_insert = property(lambda self: False)
--- studenttrack/settings.py
+++ studenttrack/settings.py
@@ -10,6 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+# Monkey-patch Django to bypass the MariaDB 10.6 version check
+from django.db.backends.base.base import BaseDatabaseWrapper
+BaseDatabaseWrapper.check_database_version_supported = lambda self: None
+
+# Monkey-patch Django's MySQL features to disable the RETURNING clause for MariaDB 10.4 compatibility
+from django.db.backends.mysql.features import DatabaseFeatures
+DatabaseFeatures.can_return_columns_from_insert = property(lambda self: False)
+DatabaseFeatures.can_return_rows_from_bulk_insert = property(lambda self: False)
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
