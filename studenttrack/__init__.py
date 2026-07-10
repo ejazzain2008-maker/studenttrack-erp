bdd340fde66f360fa835f542926bafbff7543dba
fauziaejaz9-ux <fauziaejaz9@gmail.com>
2026-07-10 17:21:37 +0530

    Bypass MariaDB version check for 10.4 compatibility

--- studenttrack/__init__.py
+++ studenttrack/__init__.py
@@ -1,2 +1,6 @@
 import pymysql
 pymysql.install_as_MySQLdb()
+
+# Monkey-patch Django to bypass the MariaDB 10.6 version check
+from django.db.backends.base.base import BaseDatabaseWrapper
+BaseDatabaseWrapper.check_database_version_supported = lambda self: None
